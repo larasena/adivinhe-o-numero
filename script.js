51ba4f55befa79735c567afeb6f59f342eefeb98
Lara Sena <laraf.sena@gmail.com>
2021-10-18 09:05:35 -0300

    Clique no botão para ver a mensagem

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
-
-let computador = 0;
-let jogador = 0;
-
-function sortear() {
-    let min = 1;
-    let max = 100;
-    let dif = max - min;
-    let aleatorio = Math.random();
-    computador = min + Math.trunc(dif * aleatorio);
+function mensagem1() {
+    window.alert('Você clicou no primeiro botão');
 }
-
-function jogar() {
-    let res = document.querySelector('#resultado');
-    jogador = Number(window.prompt('Qual é o seu palpite?'));
-    if (jogador > computador) {
-       res.innerHTML += `<p>O meu número é MENOR!</p>`;
-    } else if (jogador < computador) {
-        res.innerHTML += `<p>O meu número é MAIOR!</p>`;
-    } else {
-       res.innerHTML += `<p>Meus parabéns!! Eu tinha sorteado o número ${computador}!</p>`;
-        document.getElementById('botao').style.visibility = 'hidden';
-    }
+function mensagem2() {
+    window.alert('VOcê clicou no segundo botão');
+}
+function mensagem3() {
+    window.alert('Você clicou no terceiro botão');
 }
